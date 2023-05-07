ca82c0ed260a75722abbf13a8854febf0c7e3342
User <erik111752@yahoo.com>
2023-05-06 23:15:53 -0500

    Remove dat idgaf

--- Niggatector.cpp
+++ Niggatector.cpp
@@ -8,9 +8,8 @@ using namespace std;
 
 int main()
 {
-    string filename = "niggasdetected.txt";  // name of the file to store the counter
+    string filename = "niggasdetected.txt";
 
-    // read the current value of the counter from the file
     int counter = 0;
     ifstream infile(filename);
     if (infile.good())
@@ -19,10 +18,8 @@ int main()
     }
     infile.close();
 
-    // increment the counter
     counter++;
 
-    // save the updated counter back to the file
     ofstream outfile(filename);
     if (outfile.good())
     {
@@ -30,13 +27,10 @@ int main()
     }
     outfile.close();
 
-    // print the updated counter value
     cout << "Niggas Detected " << counter << endl;
 
-    // prompt the user to run the program again
     cout << "Press 'enter' to detect the niggas again, or press any other key to stop detecting: ";
 
-    // wait for user input
     bool should_run_again = false;
     if (cin.get() == '\n')
     {
@@ -45,7 +39,6 @@ int main()
 
     if (should_run_again)
     {
-        // start a new instance of the same program
         LPCTSTR program_name = TEXT("Niggatector.exe");
         HINSTANCE result = ShellExecute(NULL, NULL, program_name, NULL, NULL, SW_SHOW);
         if (result == NULL)
